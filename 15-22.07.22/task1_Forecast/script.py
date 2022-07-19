09783cfcd6b403762993816c5560ad874f4c9878
Arsen <xakars02@gmail.com>
2022-07-20 00:47:38 +0300

    fixed review2

--- 15-22.07.22/task1_Forecast/script.py
+++ 15-22.07.22/task1_Forecast/script.py
@@ -1,5 +1,6 @@
 import requests
 
+
 cities = ['Лондон', 'Шереметьево', 'Череповец']
 url_template = 'https://wttr.in/{}'
 payload = {
@@ -7,7 +8,8 @@ payload = {
     'lang': 'ru'
 }
 
-for city_name, city in enumerate(cities):
-    response = requests.get(url_template.format(cities[city_name]), params=payload)
+
+for city in cities:
+    response = requests.get(url_template.format(city), params=payload)
     response.raise_for_status()
     print(response.text)
