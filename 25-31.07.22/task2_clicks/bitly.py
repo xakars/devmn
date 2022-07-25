2916aa8146efebf13ba7f81b5b6555f3446922bf
Arsen <xakars02@gmail.com>
2022-07-25 16:40:44 +0300

    review fixed

--- 25-31.07.22/task2_clicks/bitly.py
+++ 25-31.07.22/task2_clicks/bitly.py
@@ -3,58 +3,64 @@ import os
 from urllib.parse import urlparse
 
 
-token = os.environ['Token']
-header = {
-    "Authorization": f"Bearer {token}",
-}
+token = os.environ['BITLY_TOKEN']
 
 
 def shorten_link(token: str, url: str) -> str:
     """Converts a long url to a Bitlink"""
     api_address = 'https://api-ssl.bitly.com/v4/bitlinks'
+    header = {
+        "Authorization": f"Bearer {token}",
+    } 
     data = {
-        "long_url": f"{url}",
+        "long_url": url,
     }
 
-    try:
-        response = requests.post(api_address, headers=header, json=data)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print("Введена неверная ссылка")
-        exit()
+    response = requests.post(api_address, headers=header, json=data)
+    response.raise_for_status()
     return response.json()["link"]
 
 
 def count_clicks(token: str, link: str) -> int:
     """Returns the click counts for the specified Bitlink"""
-    parts_bitlink = urlparse(link)
-    link = parts_bitlink.netloc + parts_bitlink.path
-    api_address = f"https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary"
+    parsed_link = urlparse(link)
+    netloc, path = parsed_link.netloc, parsed_link.path
+    api_address = f"https://api-ssl.bitly.com/v4/bitlinks/{netloc}{path}/clicks/summary"
+    header = {
+            "Authorization": f"Bearer {token}",
+    }
     payload = {
         "unit": "day",
         "units": -1
     }
 
-    try:
-        response = requests.get(api_address, params=payload, headers=header)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print("Не удалось получить информацию, проверьте правильность введенных данных")
+    response = requests.get(api_address, params=payload, headers=header)
+    response.raise_for_status()
     return response.json()["total_clicks"]
 
 
 def is_bitlink(url: str) -> bool:
     """Verifide url to fit Bitlink"""
     parsed_link = urlparse(url)
-    return "bit.ly" in parsed_link
+    netloc, path = parsed_link.netloc, parsed_link.path
+    api_address = f"https://api-ssl.bitly.com/v4/bitlinks/{netloc}{path}"
+    header = {
+        "Authorization": f"Bearer {token}",
+    }
+
+    response = requests.get(api_address, headers=header)
+    return response.ok
 
 
 if __name__ == '__main__':
     url = input("Введите ссылку: ")
-    if is_bitlink(url):
-        print("По вашей ссылке прошли:", count_clicks(token, url), "раз(а)")
-    else:
-        print("Битлинк:", shorten_link(token, url))
+    try:
+        if is_bitlink(url):
+            print("По вашей ссылке прошли:", count_clicks(token, url), "раз(а)")
+        else:
+            print("Битлинк:", shorten_link(token, url))
+    except requests.exceptions.HTTPError:
+        print("Не удалось получить информацию, проверьте правильность введенных данных")
 
 
 
