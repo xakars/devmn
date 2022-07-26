08515d01b112e240259d784e2239e8f0217c9f93
Arsen <xakars02@gmail.com>
2022-07-26 12:13:24 +0300

    review02

--- 25-31.07.22/task2_clicks/bitly.py
+++ 25-31.07.22/task2_clicks/bitly.py
@@ -3,7 +3,6 @@ import os
 from urllib.parse import urlparse
 
 
-token = os.environ['BITLY_TOKEN']
 def shorten_link(token: str, url: str) -> str:
     """Converts a long url to a Bitlink"""
     api_address = 'https://api-ssl.bitly.com/v4/bitlinks'
@@ -35,7 +34,7 @@ def count_clicks(token: str, link: str) -> int:
     return response.json()["total_clicks"]
 
 
-def is_bitlink(url: str) -> bool:
+def is_bitlink(token: str, url: str) -> bool:
     """Verifide url to fit Bitlink"""
     parsed_link = urlparse(url)
     netloc, path = parsed_link.netloc, parsed_link.path
@@ -47,10 +46,11 @@ def is_bitlink(url: str) -> bool:
     return response.ok
 
 
-if __name__ == '__main__':
+def main():
+    token = os.environ['BITLY_TOKEN']
     url = input("Введите ссылку: ")
     try:
-        if is_bitlink(url):
+        if is_bitlink(token, url):
             print("По вашей ссылке прошли:", count_clicks(token, url), "раз(а)")
         else:
             print("Битлинк:", shorten_link(token, url))
@@ -58,4 +58,6 @@ if __name__ == '__main__':
         print("Не удалось получить информацию, проверьте правильность введенных данных")
 
 
+if __name__ == '__main__':
+    main()
 
